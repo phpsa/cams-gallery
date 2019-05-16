95568d2fa565f0a14fbefd287fb940261bb2ba3c
Craig Smith <craig.smith@customd.com>
2019-05-17 09:03:43 +1200

    Gallery. published - routes update

--- src/Ams/Gallery/CategoryAsset.php
+++ src/Ams/Gallery/CategoryAsset.php
@@ -10,27 +10,34 @@ class CategoryAsset extends Asset {
     public $shortname = 'GalleryCategory';
     /* assets that can be children, a value means it is limited to that many children, -1 means unlimited  */
     public $accept = "Phpsa\CamsGallery\Ams\Gallery\PageAsset";
-    public $accept_limit = "-1";
+	public $accept_limit = "-1";
+	public $status_equals = 'status';
 
     // map our value as equal to whatever the title property is set on save.
     public $value_equals = 'category';
 
-    public $properties = array(
+    public $properties = [
         'category' => array(
             'name' => 'Category',
             'type' => self::STRING
         ),
-        'thumbnail' => array(
+        'thumbnail' => [
             'name' => 'Category Image',
             'type' => self::IMG
-        )
-    );
+		],
+		'status' => [
+            'name' => 'Published',
+            'type' => self::DROPDOWN,
+			'options' => ['published' => 'Published', 'unpublished' => 'Unpublished'],
+			'published' => ['published']
+        ]
+	];
 
     public static function about() {
         return 'This is a category to which Gallery Pages can be assigned.';
 	}
 
-		/**
+	/**
 	 * generate the route for this asset
 	 */
 	public static function route($record, $path = null){
--- src/Http/Controllers/GalleryController.php
+++ src/Http/Controllers/GalleryController.php
@@ -14,9 +14,51 @@ use Phpsa\Datastore\Models\DatastorePages;
 
 Class GalleryController extends Controller {
 
+	/**
+	 * @var DatastoreRepository
+	 */
+    protected $datastoreRepository;
+
+    /**
+     * UserController constructor.
+     *
+     * @param DatastoreRepository $datastoreRepository
+     */
+    public function __construct(DatastoreRepository $datastoreRepository)
+    {
+        $this->datastoreRepository = $datastoreRepository;
+	}
+
+	public function index(){
+
+		$galleries = $this->datastoreRepository->where('shortname','GalleryCategory')->where('status','published')->orderBy('value','asc')->paginate();
+
+		dd($galleries);
+	}
 
 	public function category($slug){
+		$page = DatastorePages::where('slug', $slug)->first();
+		if(!$page){
+			abort(404);
+		}
 
+		$datastore = $page->datastore;
+		$user = auth()->user();
+
+		if(!$datastore->statusIsActive() && (!$user || !$user->can('manage datastore'))) {
+			abort(404);
+		}
+
+
+
+		/*
+		$data['items'] = db::find(zpASSET, "id IN (
+                SELECT dd.datastore_id
+                FROM " . $tbl . " dd
+                    where dd.datastore2_id = ?
+                )
+                order by time_updated desc", array($cat_id));*/
+		dd($datastore->items);
 	}
 
 	public function page($slug){
